be778471ac5d958fe595fadd1aeeca38264fb8be
Tony Wang <twang35@outlook.com>
2015-03-19 14:12:13 -0400

    Cleaned up index.html

--- fortuna/randBeer/templatetags/rand_extras.py
+++ fortuna/randBeer/templatetags/rand_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 import requests
+import json
 
 register = template.Library()
 
@@ -11,10 +12,12 @@ from brewerydb.brewerydb import *
 BreweryDb.configure(apikey)
 
 @register.filter
-@stringfilter
-def getBeer(value, args):
-	return BreweryDb.beer(value)
+def getBeer(value):
+	return value['data']['available']
 
 @register.filter
 def getRand(value):
-	return requests.get("http://api.brewerydb.com/v2/beer/random?key=5b3814c58c765b0d58b67d3525c4850b&format=json").json()
+	decode = requests.get("http://api.brewerydb.com/v2/beer/oeGSxs?key=5b3814c58c765b0d58b67d3525c4850b&format=json").json()
+	print decode
+
+	return requests.get("http://api.brewerydb.com/v2/beer/oeGSxs?key=5b3814c58c765b0d58b67d3525c4850b&format=json").json()['data']['available']
