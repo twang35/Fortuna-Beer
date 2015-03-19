9ad9534ced74174db778fa9e39c90be3c505dd6f
Fei Gao <fgao22@emory.edu>
2015-03-19 14:46:04 -0400

    Able to pull data of random beer from API using requests(python library) and render into HTML

--- fortuna/randBeer/templatetags/rand_extras.py
+++ fortuna/randBeer/templatetags/rand_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 import requests
+import json
 
 register = template.Library()
 
@@ -15,6 +16,24 @@ BreweryDb.configure(apikey)
 def getBeer(value, args):
 	return BreweryDb.beer(value)
 
-@register.filter
-def getRand(value):
+def getRand():
 	return requests.get("http://api.brewerydb.com/v2/beer/random?key=5b3814c58c765b0d58b67d3525c4850b&format=json").json()
+	
+beerInfo = getRand()
+
+
+@register.filter
+def getName(value):
+	return beerInfo['data']['name']
+
+@register.filter
+def getStyle(value1):
+	return beerInfo['data']['style']['category']['name']
+
+@register.filter 
+def getAbv(value):
+	return beerInfo['data']['abv']
+
+@register.filter 
+def getDescription(value):
+	return beerInfo['data']['description']
