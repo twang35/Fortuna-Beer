0b6bcfbbc9e4f7f50b7614be04f0cdc0866eacb3
Tony Wang <twang35@outlook.com>
2015-03-18 22:55:39 -0400

    Templates and Views

--- fortuna/fortuna/urls.py
+++ fortuna/fortuna/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from randBeer import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fortuna.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^favorites/', views.favorites, name='favorites'),
 )
--- fortuna/randBeer/views.py
+++ fortuna/randBeer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+	return render(request, 'index.html')
+
+def favorites(request):
+	# reponse = "You are accessing favorite beers with user %s."
+	# return HttpResponse(response % user_id)
+	return HttpResponse("blah favorites")
+
