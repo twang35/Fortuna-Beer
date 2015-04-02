85bb45efd54493999fcd853df000e26e919fb62b
Tony Wang <twang35@outlook.com>
2015-04-02 15:35:34 -0400

    Beer Button ID

---
+++ testing/login_randBeer.rb
@@ -0,0 +1,14 @@
+require 'watir-webdriver'
+b = Watir::Browser.new
+b.goto 'fortuna.tony-wang.com'
+b.input(:id => 'login-username').wait_until_present
+b.text_field(:id => 'login-username').when_present.set 'a'
+b.text_field(:id => 'login-password').wait_until_present
+b.text_field(:id => 'login-password').set 'a'
+
+b.button(:id => 'btnLogIn').click
+b.button(:id => 'btnLogOut').wait_until_present
+
+b.button(:id => 'btnRandBeer').click
+
+puts "Success"
