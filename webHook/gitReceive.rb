e496641bcbfd9cf9c1aa3762ab19670e065fb49c
Tony Wang <twang35@outlook.com>
2015-04-07 19:43:09 -0400

    Git Webhook

---
+++ webHook/gitReceive.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'json'
+
+set :port, 4321
+
+post '/payload' do
+  push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+
+  # test = `ruby ../testing/login.rb`
+  # File.open("../testing/watirLog.txt", 'a') { |file| file.write("login.rb: " << test << "\n") }
+
+  # test = `ruby ../testing/login-logout.rb`
+  # File.open("../testing/watirLog.txt", 'a') { |file| file.write("login-logout.rb: " << test << "\n") }
+
+
+
+  # File.open("../testing/watirLog.txt", 'a') { |file| file.write("=====\n\n\n") }
+end
