198c1e508dbf697b62a75cf5c5ab74a736dd3840
Tony Wang <twang35@outlook.com>
2015-04-07 20:22:40 -0400

    Test Push 7

--- webHook/gitReceive.rb
+++ webHook/gitReceive.rb
@@ -3,17 +3,19 @@ require 'json'
 
 set :port, 4321
 
-$watirLog = File.open("../testing/watirLog.txt", 'a')
+$watirLog = nil
 
 def runTest(name)
 	return $watirLog.write("#{name}: " << `ruby ../testing/#{name}` << "\n")
 end
 
 post '/payload' do
+  $watirLog = File.open("../testing/watirLog.txt", 'a')
+
+  
   push = JSON.parse(request.body.read)
 
   $watirLog.write(push["commits"][0]["message"] << "\n\n")
-  
 
   runTest("login.rb")
   runTest("login_logout.rb")
