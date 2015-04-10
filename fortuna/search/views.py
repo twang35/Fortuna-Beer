34773d776ec847d3f9a955ab4073ca8d7ed8c300
fgao22 <fgao22@emory.edu>
2015-04-09 21:42:28 -0400

    Template block restructure 2

--- fortuna/search/views.py
+++ fortuna/search/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from forms import NameForm
+from randBeer.forms import AuthenticateForm, UserCreateForm, BeerForm
+
+from randBeer.models import Beer
 
 def get_name(request):
 
@@ -9,11 +12,51 @@ def get_name(request):
 		search_form = NameForm(request.GET)
 		if search_form.is_valid():
 			searchName = search_form.cleaned_data['searchBeer']
-			return render(request, 'search.html', {'search_form': search_form,
-													'searchName': searchName})
+			if request.user.is_authenticated():
+				beer_form = BeerForm()
+				user = request.user
+				beers = Beer.objects.filter(user=user.id).order_by('-creation_date')[:5]
+
+				return render(request,
+								'search.html',
+								{'user': user,
+								'beers': beers, 
+								'beer_form': beer_form, 
+								'search_form': search_form,
+								'next_url': '/', })
+			else:
+				auth_form = auth_form or AuthenticateForm()
+				user_form = user_form or UserCreateForm()
+
+				return render(request,
+						'search.html',
+						{'auth_form': auth_form,
+						'user_form': user_form, 
+						'search_form': search_form,})
 
 	else:
 		search_form = NameForm()
+		if request.user.is_authenticated():
+			beer_form = BeerForm()
+			user = request.user
+			beers = Beer.objects.filter(user=user.id).order_by('-creation_date')[:5]
+
+			return render(request,
+							'search.html',
+							{'user': user,
+							'beers': beers, 
+							'beer_form': beer_form, 
+							'search_form': search_form,
+							'next_url': '/', })
+		else:
+			auth_form = auth_form or AuthenticateForm()
+			user_form = user_form or UserCreateForm()
+
+			return render(request,
+					'search.html',
+					{'auth_form': auth_form,
+					'user_form': user_form, 
+					'search_form': search_form,})
 		
 
 	return render(request, 'search.html', {'search_form': search_form})
