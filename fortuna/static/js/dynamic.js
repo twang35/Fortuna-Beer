98e5de2cc747723cc13eec2f9bacf1ff58a78486
Tony Wang <twang35@outlook.com>
2015-04-16 03:56:58 -0400

    Asynchronous DB call w/proxy.php

--- fortuna/static/js/dynamic.js
+++ fortuna/static/js/dynamic.js
@@ -5,7 +5,23 @@ $(document).ready(function(){
    		$.get('/suggest/', {beerStyle: style}, function(data){
                $('#test').html(data);
     	});
+   	});
 
+    $('#btnRandBeer').click(function(){
+		var name;
+		name = $('#randomBeerStyle').html()
+
+		$.ajax({
+		  url: 'http://54.172.157.49/proxy.php',
+		  dataType: 'jsonp',
+		  success: function(json){
+		    $('#randomBeerName').html( 'Name: '+json.data.name );
+		    $('#randomBeerStyle').html( 'Style: '+json.data.style.category.name );
+		    $('#randomBeerAbv').html( 'ABV: '+json.data.abv );
+		    $('#randomBeerDescription').html( 'Description: '+json.data.description );
+		    $('#randomBeerLabel').html( '<img src="' + json.data.labels.medium + '" alt="Beer Label" class = "pull-right" width = "100%">');
+
+		  }
+		});
 	});
 });
-
